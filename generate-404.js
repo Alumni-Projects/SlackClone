f603a198b0571e2b3c3d325ca84247ee0aac5c48
John Fieweger <john.fieweger98@gmail.com>
2025-03-03 10:36:05 +0100

    fix: path of dist build

--- generate-404.js
+++ generate-404.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // Lies die `index.html`-Datei
-const indexHtml = fs.readFileSync('dist/slackclone/browser/index.html', 'utf8');
+const indexHtml = fs.readFileSync('dist/slack-clone/browser/index.html', 'utf8');
 
 // Kopiere `index.html` zu `404.html`
-fs.writeFileSync('dist/slackclone/browser/404.html', indexHtml);
+fs.writeFileSync('dist/slack-clone/browser/404.html', indexHtml);
