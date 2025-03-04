a4595a71e9ccc5cf8d171afbc19e4d9b1ddf4b8a
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2025-03-04 06:24:17 +0100

    components: delete icon component and integrate angular materials and adding icon enum

--- src/app/chooseavatar/chooseavatar.component.ts
+++ src/app/chooseavatar/chooseavatar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon'
+import { IconSize } from '../Enums/iconSize';
+import { Color } from '../Enums/color';
 
 @Component({
   selector: 'app-chooseavatar',
   templateUrl: './chooseavatar.component.html',
   styleUrls: ['./chooseavatar.component.css'],
+  imports: [MatIconModule],
   standalone: true
 })
 export class ChooseAvatarComponent {
+[x: string]: any;
   avatarImage = '/assets/avatar/avatar1.svg';
   userName = 'Frederik Beck'; 
+iconColor = Color;
+iconSize: string = IconSize.Medium;
+Color = Color;
 }
