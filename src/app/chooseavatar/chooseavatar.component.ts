75e65d2a4aa6a72d03950906a5f9dfbc57c3a4ff
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2025-03-05 07:05:04 +0100

    Ticket -21-breakpoints and styling

--- src/app/chooseavatar/chooseavatar.component.ts
+++ src/app/chooseavatar/chooseavatar.component.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon'
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatIconModule } from '@angular/material/icon';
 import { IconSize } from '../Enums/iconSize';
 import { Color } from '../Enums/color';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chooseavatar',
   templateUrl: './chooseavatar.component.html',
-  styleUrls: ['./chooseavatar.component.css'],
-  imports: [MatIconModule],
+  styleUrls: ['./chooseavatar.component.scss'],
+  imports: [MatIconModule, CommonModule],
   standalone: true
 })
-export class ChooseAvatarComponent {
-[x: string]: any;
-  avatarImage = '/assets/avatar/avatar1.svg';
-  userName = 'Frederik Beck'; 
-iconColor = Color;
-iconSize: string = IconSize.Medium;
-Color = Color;
+
+export class ChooseAvatarComponent implements OnInit {
+  avatarImage = '/assets/avatars/avatar1.svg';
+  userName = 'Frederik Beck';
+  iconColor = Color;
+  iconSize: string = IconSize.Medium;
+  Color = Color;
+  isMobile = false;
+
+  avatars = [
+    { src: '/assets/avatar/avatar1.svg' },
+    { src: '/assets/avatar/avatar2.svg' },
+    { src: '/assets/avatar/avatar3.svg' },
+    { src: '/assets/avatar/avatar4.svg' }
+  ];
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        this.isMobile = result.matches;
+      });
+  }
 }
