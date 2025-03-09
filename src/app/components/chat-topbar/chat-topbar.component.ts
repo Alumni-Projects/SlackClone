c7f955fbcd0a78d027bb6064f52d08f0c76d9b00
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2025-03-09 04:55:40 +0100

    fix: outsource profile component

--- src/app/components/chat-topbar/chat-topbar.component.ts
+++ src/app/components/chat-topbar/chat-topbar.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { IconSize } from '../../Enums/iconSize';
+import { Color } from '../../Enums/color';
+import { CommonModule } from '@angular/common';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-chat-topbar',
   templateUrl: './chat-topbar.component.html',
+  imports: [CommonModule, MatIconModule, ProfileComponent],
   styleUrls: ['./chat-topbar.component.scss']
 })
 export class ChatTopbarComponent {
+  iconColor = Color.Black;
+  iconSize: string = IconSize.Small;
+  Color = Color;
   onSendMessage($event: Event) {
     throw new Error('Method not implemented.');
   }
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { IconSize } from '../../Enums/iconSize';
+import { Color } from '../../Enums/color';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+imports: [MatIconModule, CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-
+  iconColor = Color.Black;
+  iconSize: string = IconSize.Small;
+  Color = Color;
 }
