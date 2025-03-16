0e7b01efe68f3e41987a825bdcbda38ec2c859e3
John TwiiX <104139228+JohnTwiiX@users.noreply.github.com>
2025-03-16 20:29:32 +0100

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-import { ChooseAvatarComponent } from './chooseavatar/chooseavatar.component'; 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations(),ChooseAvatarComponent ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimations() ]
 };
