bac119663f154fc0222467836f2e9f715ed0d313
John TwiiX <104139228+JohnTwiiX@users.noreply.github.com>
2025-03-17 08:20:49 +0100

    fix: not import individual components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DevspaceComponent } from './components/devspace/devspace.component';
-import { WorkspaceOpenCloseComponent } from './components/workspace-open-close/workspace-open-close.component';
-import { DevspaceMessageComponent } from './components/devspace-message/devspace-message.component';
-import { ChatTopbarComponent } from "./components/chat-topbar/chat-topbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DevspaceComponent, WorkspaceOpenCloseComponent, DevspaceMessageComponent, ChatTopbarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
