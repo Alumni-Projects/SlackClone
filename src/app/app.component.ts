06b04012bbec7cbd356476ed4e86602697464d85
John Fieweger <john.fieweger98@gmail.com>
2025-03-17 18:56:54 +0100

    fix: delete unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChatTopbarComponent } from './components/chat-topbar/chat-topbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChatTopbarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
