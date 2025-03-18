9ec04077005e0b9f964ba2c7d6ddcc5c60f5a0f3
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2025-03-18 10:31:20 +0100

    styling: use new button theme styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,5 +8,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
 }
