c7311ed3bacee2af979f857068dcee84c2166e4f
John Fieweger <john.fieweger98@gmail.com>
2025-03-20 14:52:23 +0100

    feat: add start animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 
 @Component({
@@ -9,5 +9,22 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  isDashboardRoute = false;
+  isSmallDisplay = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isDashboardRoute = event.urlAfterRedirects === '/dashboard';
+        if (event.urlAfterRedirects === '/login')
+          this.isSmallDisplay = window.innerHeight < 900 && window.innerWidth < 768;
+      }
+    });
+
+
+  }
+
 
 }
